be8f8d94888f25847a525c3aa9ffb5f20bf7ca99
Prev <prevdev@gmail.com>
2021-04-05 14:15:06 +0900

    Fix CI bug

--- test-ci.sh
+++ test-ci.sh
@@ -8,29 +8,32 @@ docker build -t kaistgclab/delta-processor-null .
 # https://circleci.com/docs/2.0/building-docker-images/#mounting-folders
 
 # Create a dummy container which will hold a volume for file sharing
-docker create -v /data --name volume-container kaistgclab/delta-processor-null /bin/true
+docker create \
+    -v /data \
+    --name vcontainer \
+    kaistgclab/delta-processor-null \
+    /bin/true
 
 # Copy any file (in this case, we use src/index.ts)
-docker cp src/index.ts volume-container:/data
+INPUT_FILE=index.ts
+docker cp "src/$INPUT_FILE" vcontainer:/data
 
 # Run container
 docker run \
-    --volumes-from volume-container \
+    --volumes-from vcontainer \
     --name app \
     kaistgclab/delta-processor-null \
-    -i index.ts \
+    -i "$INPUT_FILE" \
     -o output.txt
 
-mkdir output
-
-# Copy output 
+# Copy output (the entire `data` directory is copied to the current location)
 docker cp app:/data .
 
 # Check output.txt file exists
-FILE=data/output.txt
-if [ -f "$FILE" ]; then
-    echo "$FILE exists."
+OUTPUT_FILE=data/output.txt
+if [ -f "$OUTPUT_FILE" ]; then
+    echo "$OUTPUT_FILE exists."
 else 
-    echo "$FILE does not exist."
+    echo "$OUTPUT_FILE does not exist."
     exit 1
 fi
