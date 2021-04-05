15226dddd5c056655ba5ac56c17baed7c083fe57
Prev <prevdev@gmail.com>
2021-04-05 14:16:58 +0900

    CI 버그를 위해 메소드를 변경합니다

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import { copyFile } from 'fs/promises';
+import { readFileSync, writeFileSync } from 'fs';
 import { ArgumentParser } from 'argparse';
 
 const parser = new ArgumentParser({
@@ -10,12 +10,17 @@ parser.add_argument('-o', '--output', { help: 'Path to output file' });
 
 const args = parser.parse_args();
 
-const inputFileName = args['input'];
-const outputFileName = args['output'] || ('out.' + inputFileName.split('.').pop());
+const inputFileName = args['input'] as string;
+const outputFileName = args['output'] as string || ('out.' + inputFileName.split('.').pop());
 
+// Copy file.
+// Due to the permission problem, we cannot use `fs.copyFile` or `fs.copyFileSync`.
+// Instead, we read the file content and write to the output file.
+const data = readFileSync(inputFileName);
 try {
-    copyFile(inputFileName, outputFileName);
-} catch {
-    console.log('The file could not be copied');
+    writeFileSync(outputFileName, data);
+} catch (e) {
+    console.log('The file could not be written');
+    console.error(e);
     process.exit(1);
 }
